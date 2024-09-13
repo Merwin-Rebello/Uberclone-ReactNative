847dfb4c6dfe92f3b15b79d84f82ff814a1ca429
Merwin Rebello <merwinrebello02@gmail.com>
2024-09-14 01:15:28 +0530

    inital commit

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,24 @@
+
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
+
+
+const Layout = () => {
+  
+
+  return (
+    
+      <Stack>
+        <Stack.Screen name="welcome" options={{ headerShown: false }} />
+        <Stack.Screen name="sign-up" options={{ headerShown: false }} />
+        <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+
+    
+
+      </Stack>
+    
+  );
+}
+export default Layout;
---
+++ app/(root)/(tabs)/history.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const History = () => {
+  return (
+    <SafeAreaView> 
+        <Text>History</Text>
+        </SafeAreaView>
+     
+  
+  )
+}
+
+export default History
